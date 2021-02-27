ad78e2160d004fec0ab7adfc62f1937fd4ee670c
Gustavo Trindade <gustavo96634075@gmail.com>
2021-02-27 16:35:48 -0300

    ultima revisada

--- src/index.js
+++ src/index.js
@@ -5,248 +5,256 @@ import css from "./style.css"
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
     var self = this
-   
+
     //nomee
-    self.firstName = ko.observable().extend({  
-        required:{
-            params:true,
-            message: 'campo obrigatorio'
-    },
-        minLength:{
-            params:2,
-            message: 'Caracteres insuficientes'
-    },
-        pattern:{
-            params:"^[A-Za-zÀ-ú ']+$",
-            message: 'Apenas letras'
-    }}),
-    //sobrenome
-     self.lastName = ko.observable().extend({  
-        required:{
-            params:true,
-            message: 'campo obrigatorio'
-    },
-        minLength:{
-            params:2,
-            message: 'Caracteres insuficientes'
-    },
-        pattern:{
-            params:"^[A-Za-zÀ-ú ']+$",
-            message: 'apenas letras'
-    }}),
-    //ddd
-    self.ddd = ko.observable().extend({  
-        required:{
-            params:true,
-            message: 'campo obrigatorio'
-    },
-        minLength:{
-            params:2,
-            message: 'Caracteres insuficientes'
-    },
-        maxLength:{
-            params:3,
-            message: ' Caracteres excedidos'
-    },
-        number:{
-            params:true,
-            message: 'Apenas numeros'
-    }}),
-    //telefonee
-
-     self.numberFone = ko.observable().extend({ 
+    self.firstName = ko.observable().extend({
         required: {
-            params:true,
+            params: true,
             message: 'campo obrigatorio'
-    },
-        minLength:{
-            params:9,
+        },
+        minLength: {
+            params: 2,
             message: 'Caracteres insuficientes'
-    },
-        maxLength:{
-            params:9,
-            message: ' Caracteres excedidos'
-    },
-        number:{
-            params:true,
-            message: 'Apenas numeros'
-    }}),
-    // cep
-     self.cep = ko.observable().extend({ 
-        required:{
-            params:true,
-            message: 'campo obrigatorio'
-    },
-        minLength:{
-            params:8,
-            message: ' Caracteres insuficientes'
-    },
-        maxLength:{
-            params:8,
-            message: ' Caracteres excedidos'
-    },
-        number:{
-            params:true,
-            message: 'Apenas numeros'
-    }})
+        },
+        pattern: {
+            params: "^[A-Za-zÀ-ú ']+$",
+            message: 'Apenas letras'
+        }
+    }),
+        //sobrenome
+        self.lastName = ko.observable().extend({
+            required: {
+                params: true,
+                message: 'campo obrigatorio'
+            },
+            minLength: {
+                params: 2,
+                message: 'Caracteres insuficientes'
+            },
+            pattern: {
+                params: "^[A-Za-zÀ-ú ']+$",
+                message: 'apenas letras'
+            }
+        }),
+        //ddd
+        self.ddd = ko.observable().extend({
+            required: {
+                params: true,
+                message: 'campo obrigatorio'
+            },
+            minLength: {
+                params: 2,
+                message: 'Caracteres insuficientes'
+            },
+            maxLength: {
+                params: 3,
+                message: ' Caracteres excedidos'
+            },
+            number: {
+                params: true,
+                message: 'Apenas numeros'
+            }
+        }),
+        //telefonee
+
+        self.numberFone = ko.observable().extend({
+            required: {
+                params: true,
+                message: 'campo obrigatorio'
+            },
+            minLength: {
+                params: 9,
+                message: 'Caracteres insuficientes'
+            },
+            maxLength: {
+                params: 9,
+                message: ' Caracteres excedidos'
+            },
+            number: {
+                params: true,
+                message: 'Apenas numeros'
+            }
+        }),
+        // cep
+        self.cep = ko.observable().extend({
+            required: {
+                params: true,
+                message: 'campo obrigatorio'
+            },
+            minLength: {
+                params: 8,
+                message: ' Caracteres insuficientes'
+            },
+            maxLength: {
+                params: 8,
+                message: ' Caracteres excedidos'
+            },
+            number: {
+                params: true,
+                message: 'Apenas numeros'
+            }
+        })
     // endereço
-            self.endereço = ko.observable().extend({
-                required:{
-                    params:true,
-                    message: 'campo obrigatorio'
+    self.endereço = ko.observable().extend({
+        required: {
+            params: true,
+            message: 'campo obrigatorio'
+        },
+        minLength: {
+            params: 5,
+            message: 'Caracteres insuficientes'
+        },
+        pattern: {
+            params: "^[A-Za-zÀ-ú0-9 ']+$",
+            message: 'Apenas letras'
+        }
+    }),
+        // numero da casa
+        self.nResidencia = ko.observable().extend({
+            required: {
+                params: true,
+                message: 'campo obrigatorio'
             },
-                minLength:{
-                    params:5,
-                    message: 'Caracteres insuficientes'
+            number: {
+                params: true,
+                message: 'Apenas numeros'
+            }
+        }),
+        // complemento (opcional)
+        self.complemento = ko.observable().extend({
+            pattern: {
+                params: "^[A-Za-zÀ-ú0-9 ']+$",
+                message: 'apenas letras'
+            }
+        }),
+        //bairro
+        self.bairro = ko.observable().extend({
+            required: {
+                params: true,
+                message: 'campo obrigatorio'
             },
-                pattern:{
-                    params:"^[A-Za-zÀ-ú0-9 ']+$",
-                    message: 'Apenas letras'
-            }}),
-            // numero da casa
-            self.nResidencia = ko.observable().extend({ 
-                required:{
-                    params:true,
-                    message: 'campo obrigatorio'
-            },
-                number:{
-                    params:true,
-                    message: 'Apenas numeros'
-            }}),
-            // complemento (opcional)
-            self.complemento = ko.observable().extend({  
-                pattern:{
-                    params:"^[A-Za-zÀ-ú0-9 ']+$",
-                    message: 'apenas letras'
-            }}),
-            //bairro
-            self.bairro = ko.observable().extend({
-                required:{
-                    params:true,
-                    message: 'campo obrigatorio'
-            },
-            minLength:{
-                params:3,
+            minLength: {
+                params: 3,
                 message: 'Caracteres insuficientes'
             },
-            pattern:{
-                params:"^[A-Za-zÀ-ú0-9 ']+$",
+            pattern: {
+                params: "^[A-Za-zÀ-ú0-9 ']+$",
                 message: 'Caracter invalido'
-            }}),
-            //cidade
-           
-            self.cidade = ko.observable().extend({
-                required:{
-                    params:true,
-                    message: 'campo obrigatorio'
+            }
+        }),
+        //cidade
+
+        self.cidade = ko.observable().extend({
+            required: {
+                params: true,
+                message: 'campo obrigatorio'
             },
-            minLength:{
-                params:3,
+            minLength: {
+                params: 3,
                 message: 'Caracteres insuficientes'
             },
-            pattern:{
-                params:"^[A-Za-zÀ-ú0-9 ']+$",
+            pattern: {
+                params: "^[A-Za-zÀ-ú0-9 ']+$",
                 message: 'Caracter invalidos'
-            }}),     
-            //estado
+            }
+        }),
+        //estado
 
-            self.estado = ko.observable().extend({
-                required:{
-                    params:true,
-                    message: 'campo obrigatorio'
+        self.estado = ko.observable().extend({
+            required: {
+                params: true,
+                message: 'campo obrigatorio'
             },
-            minLength:{
-                params:2,
+            minLength: {
+                params: 2,
                 message: 'Caracteres insuficientes'
             },
-            pattern:{
-                params:"^[A-Za-zÀ-ú ']+$",
+            pattern: {
+                params: "^[A-Za-zÀ-ú ']+$",
                 message: 'Apenas letras'
-            }})
+            }
+        })
 
-            self.travaCep = ko.observable(true)
-            self.travaEndereço = ko.observable(true)
-            self.travaEstado = ko.observable(true)
-            self.travaBairro = ko.observable(true)
-            self.travaCidade = ko.observable(true)
+    self.travaCep = ko.observable(true)
+    self.travaEndereço = ko.observable(true)
+    self.travaEstado = ko.observable(true)
+    self.travaBairro = ko.observable(true)
+    self.travaCidade = ko.observable(true)
 
 
-    var  Erros = ko.validation.group([self.firstName,self.lastName,self.ddd,self.numberFone,self.cep])
+    var Erros = ko.validation.group([self.firstName, self.lastName, self.ddd, self.numberFone, self.cep])
 
-    self.testCep = function(){
-            var cep = appViewModel.cep()
+    self.testCep = function () {
+        var cep = appViewModel.cep()
 
-            if(Erros().length == 0){
+        if (Erros().length == 0) {
 
-                api.getCep(cep).then((busca) => {
-                
-                    console.log(busca)
-                    self.cidade(busca.localidade)
-                    self.estado(busca.uf)
-                    self.bairro(busca.bairro)
-                    self.endereço(busca.logradouro) 
+            api.getCep(cep).then((busca) => {
 
-                    if(self.cep()!= ""){
-                        self.travaCep(false)
-                    }else{
-                        self.travaCep(true)
-                    }if(self.endereço()!= ""){
-                        self.travaEndereço(false)
-                    }else{
-                        self.travaEndereço(true)
-                    }if(self.estado()!= ""){
-                        self.travaEstado(false)
-                    }else{
-                        self.travaEstado(true)
-                    }if(self.cidade()!= ""){
-                        self.travaCidade(false)
-                    }else{
-                        self.travaCidade(true)
-                    }if(self.bairro()!= ""){
-                        self.travaBairro(false)
-                    }else{
-                        self.travaBairro(true)
-                    }
-                    
+                console.log(busca)
+                self.cidade(busca.localidade)
+                self.estado(busca.uf)
+                self.bairro(busca.bairro)
+                self.endereço(busca.logradouro)
 
-                    
-                })
-            }else{
-        Erros.showAllMessages()
-    }  
-}  
+                if (self.cep() != "") {
+                    self.travaCep(false)
+                } else {
+                    self.travaCep(true)
+                } if (self.endereço() != "") {
+                    self.travaEndereço(false)
+                } else {
+                    self.travaEndereço(true)
+                } if (self.estado() != "") {
+                    self.travaEstado(false)
+                } else {
+                    self.travaEstado(true)
+                } if (self.cidade() != "") {
+                    self.travaCidade(false)
+                } else {
+                    self.travaCidade(true)
+                } if (self.bairro() != "") {
+                    self.travaBairro(false)
+                } else {
+                    self.travaBairro(true)
+                }
+            })
+        } else {
+            Erros.showAllMessages()
+        }
+    }
 
-    self.enviaFormulario = function(){
+    self.enviaFormulario = function () {
 
-    self.Erros = ko.validation.group([self.firstName, self.lastName, self.numberFone, self.ddd, self.cep, self.endereço, self.nResidencia, self.bairro, self.cidade, self.estado])
-    if(self.Erros().length == 0){
-        $('#botao').removeAttr('disabled')
+        self.Erros = ko.validation.group([self.firstName, self.lastName, self.numberFone, self.ddd, self.cep, self.endereço, self.nResidencia, self.bairro, self.cidade, self.estado])
+        if (self.Erros().length == 0) {
+            $('#botao').removeAttr('disabled')
             var objJson = {
                 'firstName': self.firstName(),
-                'lastName' : self.lastName(),
-                'phone' : self.numberFone(),
+                'lastName': self.lastName(),
+                'phone': self.numberFone(),
                 'cep': self.cep(),
-                'address' : self.endereço(),
-                'number' : self.nResidencia(),
-                'complement' : self.complemento(),
+                'address': self.endereço(),
+                'number': self.nResidencia(),
+                'complement': self.complemento(),
                 'district': self.bairro(),
-                'city' : self.cidade(),
+                'city': self.cidade(),
                 'state': self.estado()
-        } 
-        console.log(objJson)
-    }else{
-        $('#botao').attr('disabled','disabled') 
+            }
+            console.log(objJson)
+        } else {
+            $('#botao').attr('disabled', 'disabled')
+        }
     }
 }
-}
 
-            
+
 const appViewModel = new AppViewModel()
 
 // can be used in the navigation console
 window.appViewModel = appViewModel
 
-appViewModel.isValid = ko.computed(function(){
+appViewModel.isValid = ko.computed(function () {
     return ko.validatedObservable(appViewModel).isValid();
 })
 
